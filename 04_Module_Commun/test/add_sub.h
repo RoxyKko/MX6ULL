93cffb09110e02f514ca36358cba12f4d60b85d3
Noah <njy_roxy@outlook.com>
2023-04-27 08:20:35 -0700

    :boom: 测试用模块

---
+++ 04_Module_Commun/test/add_sub.h
@@ -0,0 +1,20 @@
+/********************************************************************************
+ *      Copyright:  (C) 2023 Noah<njy_roxy@outlook.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  add_sub.h
+ *    Description:  This file 
+ *
+ *        Version:  1.0.0(2023年04月18日)
+ *         Author:  Noah <njy_roxy@outlook.com>
+ *      ChangeLog:  1, Release initial version on "2023年04月18日 06时32分48秒"
+ *                 
+ ********************************************************************************/
+
+#ifndef _ADD_SUB_H_
+#define _ADD_SUB_H_
+
+long add_integer(long a, long b);
+long sub_integer(long a, long b);
+
+#endif
